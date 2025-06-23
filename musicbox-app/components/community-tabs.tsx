41cacd3fed80a30edc3bbc7daf350482463cff79
cgmuniz <cauamuniz00@gmail.com>
2025-06-23 01:43:09 -0300

    fix params

--- musicbox-app/components/community-tabs.tsx
+++ musicbox-app/components/community-tabs.tsx
@@ -5,30 +5,33 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { FriendsTab } from "@/components/community-tabs/friends-tab"
 import { FollowedTab } from "@/components/community-tabs/followed-tab"
 import { GeneralTab } from "@/components/community-tabs/general-tab"
+import { Suspense } from "react"
 
 export function CommunityTabs() {
   const searchParams = useSearchParams()
   const defaultTab = searchParams.get("tab") || "friends"
 
   return (
-    <Tabs defaultValue={defaultTab} className="w-full">
-      <TabsList className="grid w-full grid-cols-3">
-        <TabsTrigger value="friends">Friends</TabsTrigger>
-        <TabsTrigger value="followed">Following</TabsTrigger>
-        <TabsTrigger value="general">General</TabsTrigger>
-      </TabsList>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Tabs defaultValue={defaultTab} className="w-full">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="friends">Friends</TabsTrigger>
+          <TabsTrigger value="followed">Following</TabsTrigger>
+          <TabsTrigger value="general">General</TabsTrigger>
+        </TabsList>
 
-      <TabsContent value="friends" className="mt-6">
-        <FriendsTab />
-      </TabsContent>
+        <TabsContent value="friends" className="mt-6">
+          <FriendsTab />
+        </TabsContent>
 
-      <TabsContent value="followed" className="mt-6">
-        <FollowedTab />
-      </TabsContent>
+        <TabsContent value="followed" className="mt-6">
+          <FollowedTab />
+        </TabsContent>
 
-      <TabsContent value="general" className="mt-6">
-        <GeneralTab />
-      </TabsContent>
-    </Tabs>
+        <TabsContent value="general" className="mt-6">
+          <GeneralTab />
+        </TabsContent>
+      </Tabs>
+    </Suspense>
   )
 }
